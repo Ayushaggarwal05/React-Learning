8c281d92d03988bbf3c4d0cf8122d438021da5b5
Ayush aggarwal <aayushaggarwal348@gmail.com>
2025-01-14 00:36:28 +0530

    connecting my page to a local server through NPM

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-// const heading = React.createElement("h1" , {} , "Hello World By React!!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const root = ReactDOM.createRSoot(document.getElementById("root"));
-// root.render(heading);
-
-
-// const head = document.createElement("h1");
-// head.innerHTML()="Hi this my file";
-// const data = document.getElementById("root");
-// data.appendChild(head);
 
 const  parent = React.createElement("div" , {id : "parent"} , [
     React.createElement("div", {id:"child1"},[
-        React.createElement("h1" , {} , "This is h1 tag from React"),
-        React.createElement("h2" , {} , "This is h2 tag from React")
+        React.createElement("h1" , {} , "I am Learning React"),
+        React.createElement("h2" , {} , "From Namaste React")
     ]),
     React.createElement("div", {id:"child2"},[
         React.createElement("h1" , {} , "This is h1 tag from React"),
         React.createElement("h2" , {} , "This is h1 tag from React")
     ])
-])
+]);
 
 
 const heading = React.createElement("h1" , null , "Hello World By React!!");
